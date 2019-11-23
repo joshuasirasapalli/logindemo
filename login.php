d5035883798a7e6d6971d82b8e1994a22f20aa64
joshuasirasapalli <45114839+joshuasirasapalli@users.noreply.github.com>
2019-11-23 16:27:11 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,3 +9,40 @@
 <?php
 require('db.php');
 session_start();
+// If form submitted, insert values into the database.
+if (isset($_POST['username'])){
+        // removes backslashes
+	$username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+	$username = mysqli_real_escape_string($con,$username);// "escapes" special characters so that MySQL interprets them as literal string characters rather than operators in the query.
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	//Checking is user existing in the database or not
+        $query = "SELECT * FROM `users` WHERE username='$username'
+and password='".md5($password)."'";
+	$result = mysqli_query($con,$query) or die(mysql_error());//Performs a query on the database
+	//die:Print a message and terminate the current script
+	$rows = mysqli_num_rows($result);//The mysqli_num_rows function is used to get the number of rows returned from a select query.
+        if($rows==1){
+	    $_SESSION['username'] = $username;
+            // Redirect user to index.php
+	    header("Location: index.php");
+         }else{
+	echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+	}
+    }else{
+?>
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+</div>
+<?php } ?>
+</body>
+</html>
