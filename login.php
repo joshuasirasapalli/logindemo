de9d07733f14cb5716583c534b36b224c788d38e
joshuasirasapalli <45114839+joshuasirasapalli@users.noreply.github.com>
2019-11-23 16:25:50 +0530

    login page intial 11 lines of code

--- login.php
+++ login.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<?php
+require('db.php');
+session_start();
